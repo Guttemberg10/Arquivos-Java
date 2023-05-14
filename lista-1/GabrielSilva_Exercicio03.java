bf55ddd2dab6e940d3ea399e0aaffd6b470b99e1
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-14 17:52:26 -0300

    GabrielSilva_Exercicio03.java

--- lista-1/GabrielSilva_Exercicio03.java
+++ lista-1/GabrielSilva_Exercicio03.java
@@ -1,1 +1,15 @@
-as
+/* Algorítmo que calcula a cotação do dólar
+   Gabriel Novais
+   05/05/2023
+ */
+import java.util.Scanner;
+
+public class ex3{
+    public static void main(String[] args){
+        Scanner leia = new Scanner(System.in);
+        double dolar = 4.95, real = 0;// Inicializando variáveis
+        System.out.print("Digite um valor em real: ");
+        real = leia.nextDouble();// Lendo uma quantia monetária em real
+        System.out.print("\nO valor em dólar é de US$"+(real / dolar));// Imprimindo e calculando a conversão
+    }
+}
