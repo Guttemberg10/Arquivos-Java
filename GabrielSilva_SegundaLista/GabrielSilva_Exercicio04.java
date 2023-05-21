a9e57d757830e22f28f9bf4e86004e62c54fe208
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-21 14:57:06 -0300

    GabrielSilva_Exercicio04.java

--- GabrielSilva_SegundaLista/GabrielSilva_Exercicio04.java
+++ GabrielSilva_SegundaLista/GabrielSilva_Exercicio04.java
@@ -7,13 +7,14 @@ import java.util.Scanner;
 public class GabrielSilva_Exercicio04{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
-        int a, b, c = 0;
+        int a, b, c = 0;// Declarando as variáveis
+       
         System.out.print("Digite um número: ");
         a = leia.nextInt();
         System.out.print("\nDigite mais um número: ");
         b = leia.nextInt();
-        System.out.print("\n A valia: "+a+"\nB valia: "+b);
-        c = a; a = b; b = c;
-        System.out.println("\n A vale: "+a+"\nB vale: "+b);
+        System.out.print("\n A valia: "+a+"\nB valia: "+b);// Imprimindo os valores digitados
+        c = a; a = b; b = c;// Invertendo o valor de "a" e de "b"
+        System.out.println("\n A vale: "+a+"\nB vale: "+b);// Imprimindo os valores alterados
     }
 }
