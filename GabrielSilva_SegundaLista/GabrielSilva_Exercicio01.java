55d6f9886c0bb7a0ba7517d822af6363fa409d7b
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-21 14:53:52 -0300

    GabrielSilva_Exercicio01.java

--- GabrielSilva_SegundaLista/GabrielSilva_Exercicio01.java
+++ GabrielSilva_SegundaLista/GabrielSilva_Exercicio01.java
@@ -8,8 +8,9 @@ public class GabrielSilva_Exercicio01{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
         double valor = 0.0;
+        
         System.out.print("Digite um valor em R$");
-        valor = leia.nextDouble();
-        System.out.print("\nValor original: R$"+valor+"\nValor com desconto: R$"+(valor - (0.15 * valor)));
+        valor = leia.nextDouble();// Lendo o valor do produto:
+        System.out.print("\nValor original: R$"+valor+"\nValor com desconto: R$"+(valor - (0.15 * valor)));// Imprimindo e calculando o desconto
     }
 }
