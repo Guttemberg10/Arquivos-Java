f78d012a165eb291132ecdd0392b5b04fd0603df
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-21 14:59:14 -0300

    GabrielSilva_Exercicio05.java

--- GabrielSilva_SegundaLista/GabrielSilva_Exercicio05.java
+++ GabrielSilva_SegundaLista/GabrielSilva_Exercicio05.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 public class GabrielSilva_Exercicio05{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
-        int n = 0;
+        int n = 0;// Declarando uma variável n
+       
         System.out.print("Digite um número: ");
-        n = leia.nextInt();
+        n = leia.nextInt();// lendo um número (n)
+        //Imprimindo e calculando o antecessor e o sucessor do número lido:
         System.out.println("Antecessor: "+(n - 1)+"\nSucessor: "+(n + 1));
     }
 }
