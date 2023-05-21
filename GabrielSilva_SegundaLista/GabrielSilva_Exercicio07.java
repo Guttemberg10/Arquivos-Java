8594312bea922dd04ae5c1b300b4c354ad982ddf
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-21 15:02:07 -0300

    GabrielSilva_Exercicio07.java

--- GabrielSilva_SegundaLista/GabrielSilva_Exercicio07.java
+++ GabrielSilva_SegundaLista/GabrielSilva_Exercicio07.java
@@ -1,4 +1,4 @@
-/* Algorítmo que testanum número
+/* Algorítmo que testa um número
    Gabriel Novais
    05/05/2023
  */
@@ -8,11 +8,12 @@ public class GabrielSilva_Exercicio07{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
         int x = 0;
+       
         System.out.print("Digite um número: ");
         x = leia.nextInt();
-        if(x > 94 && x < 456)
-            System.out.print("Acertou");
+        if(x > 94 && x < 456)// Verificando o número digitado pelo usuário
+            System.out.print("Acertou");// Imprimindo se acertou
         else
-            System.out.print("Tente de novo");
+            System.out.print("Tente de novo");// Imprimindo se errou
     }
 }
