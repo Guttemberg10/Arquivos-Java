56d53a75cd3ba96de0097a3c1dcb765c7f716017
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-21 14:56:02 -0300

    GabrielSilva_Exercicio03.java

--- GabrielSilva_SegundaLista/GabrielSilva_Exercicio03.java
+++ GabrielSilva_SegundaLista/GabrielSilva_Exercicio03.java
@@ -8,10 +8,11 @@ public class GabrielSilva_Exercicio03{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
         double b, h = 0.0;
+       
         System.out.print("Digite a base do triângulo: ");
-        b = leia.nextDouble();
+        b = leia.nextDouble();// Lendo a base do triângulo
         System.out.print("\nDigite a altura do triângulo: ");
-        h = leia.nextDouble();
-        System.out.print("\nA área do triângulo é "+((b * h) / 2));
+        h = leia.nextDouble();// Lendo a algura do triângulo
+        System.out.print("\nA área do triângulo é "+((b * h) / 2));// Imprimindo e calculando a área
     }
 }
