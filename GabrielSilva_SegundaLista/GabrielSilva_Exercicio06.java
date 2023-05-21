b5f1277164c2fce96c69df7ef7a25ad360447535
Guttemberg <125081437+Guttemberg10@users.noreply.github.com>
2023-05-21 15:01:05 -0300

    GabrielSilva_Exercicio06.java

--- GabrielSilva_SegundaLista/GabrielSilva_Exercicio06.java
+++ GabrielSilva_SegundaLista/GabrielSilva_Exercicio06.java
@@ -7,13 +7,17 @@ import java.util.Scanner;
 public class GabrielSilva_Exercicio06{
     public static void main(String[] args){
         Scanner leia = new Scanner(System.in);
-        int q_kiwi, q_laranja, q_roma = 0;
+        int q_kiwi, q_laranja, q_roma = 0;// Variáveis para a quantidade de cada sabor de sorvete
+       
+        // Lendo a quantidade de cada sabor de sorvete que o cliente comprou:
         System.out.print("Digite quantos picolés de Kiwi você comprou: ");
         q_kiwi = leia.nextInt();
         System.out.print("Digite quantos picolés de Laranja você comprou: ");
         q_laranja = leia.nextInt();
         System.out.print("Digite quantos picolés de Romã você comprou: ");
         q_roma = leia.nextInt();
+       
+        // Imprimindo e calculando o valor total da compra:
         System.out.print("Valor total da compra: "+((q_kiwi * 3.2) + (q_laranja * 2.39) + (q_roma * 7.99)));
     }
 }
